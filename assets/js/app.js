cb0ec6f8db8b3fd112fd7897e32d42abc184681e
teejayx6-glitch <77420961+teejayx6-glitch@users.noreply.github.com>
2021-07-15 12:08:08 +0530

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+	parent: document.querySelector('.distortion'),
+	intensity: 0.2,
+	image1: 'assets/img/32.jpeg',
+	image2: 'assets/img/3.jpeg',
+	displacementImage: 'assets/img/heightMap.png'
+});
